42c26ae9103950fdc911244b6c9588075d8f2aa6
Nikita Khvorostin <khvorostinn@gmail.com>
2023-11-17 01:11:05 +0300

    Working procces

--- main/src/components/mainpage/MainPage.tsx
+++ main/src/components/mainpage/MainPage.tsx
@@ -5,16 +5,18 @@ export default function MainPage(){
     return(
         <>
             <div class={styles.containerMain}>
-                <div class={styles.upakovka}>
-                    <h1 class={styles.PartOne}>UP</h1>
-                    <h1 class={styles.PartTwo}>AKOVKA</h1>
-                </div>
-                <div class={styles.info}>
-                    <div class={clsx(styles.box, styles.One)}>
-                        <p class={styles.text}>Свечи</p>
+                <div class={styles.containerSection}>
+                    <div class={styles.upakovka}>
+                        <h1 class={styles.up}>UP</h1>
+                        <h1 class={styles.akovka}>AKOVKA</h1>
                     </div>
-                    <div class={clsx(styles.box, styles.Two)}>
-                        <p class={styles.text}>Свечи</p>
+                    <div class={styles.info}>
+                        <div class={clsx(styles.box, styles.One)}>
+                            <p class={styles.text}>Свечи</p>
+                        </div>
+                        <div class={clsx(styles.box, styles.Two)}>
+                            <p class={styles.text}>Свечи</p>
+                        </div>
                     </div>
                 </div>
             </div>
